6e47615d79539c8af3fe9afdac7b487d095442b9
Ortiz Pergher <ortiz.pergher@growdev.com.br>
2022-03-01 14:06:25 -0300

    add functions to create, list, change user

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,16 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const userProfile = this.showUserProfileUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(userProfile);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
